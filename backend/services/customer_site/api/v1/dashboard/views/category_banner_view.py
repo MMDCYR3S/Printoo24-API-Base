b19dfcb8bb0315209a3d8006f3030534f1e8058b
CYR3S <amingholami06@gmail.com>
2025-12-21 16:18:32 +0330

    رفع باگ

--- backend/services/customer_site/api/v1/dashboard/views/category_banner_view.py
+++ backend/services/customer_site/api/v1/dashboard/views/category_banner_view.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 
-from shared_libs.core.domain.catalog.category import ProductCategoryDomainService
+from core.domain.catalog.category import ProductCategoryDomainService
 from ..serializers.general_serializers import (
     ProductCategoryDashboardSerializer,
     ParentCategoryListSerializer,
