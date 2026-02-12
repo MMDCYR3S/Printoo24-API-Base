3c75dea29fa3f364ef32f6ec6a8cf1a114e3bf69
CYR3S <amingholami06@gmail.com>
2026-02-12 21:44:08 +0330

    تنظیمات مربوط به استقرار

--- backend/services/customer_site/customer_site/settings/production.py
+++ backend/services/customer_site/customer_site/settings/production.py
@@ -17,6 +17,21 @@ CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     'http://185.213.165.186:3000'
 ])
 
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'x-guest-token',
+]
+
+
 CORS_ALLOW_CREDENTIALS = True
 
 # Security Headers
