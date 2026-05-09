683a2c1722438d672c0b0d9264991a9554585685
CYR3S <amingholami06@gmail.com>
2026-05-09 11:33:40 +0330

    تغییر تنظیمات استقرار :)

--- backend/services/customer_site/apps/shop/admin.py
+++ backend/services/customer_site/apps/shop/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 
 from core.models import (
     ProductField, ProductFieldChoice, ProductCommentChoices,
-    ProductFieldCondition, ProductFormula, Product
+    ProductFieldCondition, ProductFormula, Product, FieldDictionary, FieldChoiceDictionary
 )
 
 admin.site.register(ProductFormula)
 admin.site.register(ProductField)
+admin.site.register(FieldDictionary)
+admin.site.register(FieldChoiceDictionary)
 admin.site.register(ProductFieldCondition)
 admin.site.register(ProductFieldChoice)
 admin.site.register(Product)
